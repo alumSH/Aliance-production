424d0c5571145fabe696e30bdbf2c558f954c468
alumSH <alexeyshiyanov1112@gmail.com>
2026-02-04 11:56:16 +0800

    fix: logo svg

--- js/main.js
+++ js/main.js
@@ -2,11 +2,15 @@ const navbar =
   document.querySelector(
     ".navbar",
   ); /* обращаемся к документу, чтобы он через селектор нашел класс навбар и поместил его в константу navbar */
+const logo = document.querySelector(".logo-svg use");
 window.addEventListener("scroll", () => {
   if (this.scrollY > 1) {
     navbar.classList.add("navbar-light");
+    logo.href.baseVal = "img/sprite.svg#logo";
+    /*console.log(logo.href); /* узнаем какой хреф стоит у лого */
   } else {
     navbar.classList.remove("navbar-light");
+    logo.href.baseVal = "img/sprite.svg#header1";
   }
 
   /*console.log("Прокрутка");*/
